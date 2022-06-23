6faa532885c597a18449780631568b18e17bc311
Fuki3 <sooooktmmi@gmail.com>
2022-06-23 23:26:11 +0900

    厳密等価演算子を使用するように修正

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-var sortNumber = finction (number) { number.sort(function (a, b) { if (a == b) {return 0; } return a < b ? -1 : 1; }); };
+var sortNumber = finction (number) { number.sort(function (a, b) { if (a === b) {return 0; } return a < b ? -1 : 1; }); };
 
 var number = [19, 3, 81, 1, 24, 21];
 sortNumber(number);
